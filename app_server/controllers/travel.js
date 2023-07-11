4bb133cb4ff5397ab8f9f2dcec8e16bbe0fa82db
pruitttylerl <tyler4@hotmail.com>
2023-07-11 16:42:59 -0400

    Module 3: Static HTML

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,10 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
     console.log('Inside app_server, controllers, travel.js, travel function.')
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips });
    };
    module.exports = {
     travel
